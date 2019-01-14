8d16b9acd488919ea4305d6cc500f4b140c2f1d7
Jonathan Lin <jlin1999@gmail.com>
2019-01-14 18:56:44 -0500

    yAxis Initial

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,42 +1,59 @@
 #include <Servo.h>
 
-const int servoHorizontal = 5;//Horizontal servo
+const int xServoPin = 5;//Horizontal servo
+const int yServoPin = 6;//Vertical servo
 
 int xSensitivity = 40;
 
-Servo servoH;
+Servo xServo;
+Servo yServo;
 
 void setup() {
-  servoH.attach(servoHorizontal);
+  xServo.attach(xServoPin);
+  yServo.attach(yServoPin);
+
+  yServo.write(90);
 
   Serial.begin(9600);
 }
 
 void cw(int deg)
 {
-  servoH.writeMicroseconds(1500 - abs(15/100.0*200.0));  // Clockwise
+  xServo.writeMicroseconds(1500 - abs(15/100.0*200.0));  // Clockwise
   delay(double(deg)/360*4.0*1000);//One full rotation takes exactly 4s
-  servoH.writeMicroseconds(1500);  // Stop
+  xServo.writeMicroseconds(1500);  // Stop
 }
 
 void ccw(int deg)
 {
-  servoH.writeMicroseconds(1500 + abs(15/100.0*200.0));  // Clockwise
+  xServo.writeMicroseconds(1500 + abs(15/100.0*200.0));  // Clockwise
   delay(double(deg)/360*4.0*1000);//One full rotation takes exactly 4s
-  servoH.writeMicroseconds(1500);  // Stop
+  xServo.writeMicroseconds(1500);  // Stop
 }
 
 int xState = 0;
 int yState = 0;
 
+double xPos = 0.0;
+double yPos = 90.0;
+
 void loop() {
 
+  loop_function();
+  
+}
+
+void loop_function()
+{
   read_joystick_data();
   print_joystick_data();
 
-  servoH.writeMicroseconds(1500 + (xState/100.0*xSensitivity));  // Clockwise
+  xServo.writeMicroseconds(1500 + (xState/100.0*xSensitivity));
+  yServo.write(yPos);
 
-  delay(10);
+  Serial.println(yPos);
+
+  delay(100);
 }
 
 void read_joystick_data()
@@ -48,6 +65,8 @@ void read_joystick_data()
     xState = 0;
   if (abs(yState) < 5)
     yState = 0;
+
+  yPos += yState*0.5;
 }
 
 void print_joystick_data()
