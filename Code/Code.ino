40bca565dfc55e1519192603c2530cab280072ff
Jonathan Lin <jlin1999@gmail.com>
2019-01-14 18:11:55 -0500

    xAxis Code

--- Code/Code.ino
+++ Code/Code.ino
@@ -2,6 +2,8 @@
 
 const int servoHorizontal = 5;//Horizontal servo
 
+int xSensitivity = 40;
+
 Servo servoH;
 
 void setup() {
@@ -24,9 +26,36 @@ void ccw(int deg)
   servoH.writeMicroseconds(1500);  // Stop
 }
 
+int xState = 0;
+int yState = 0;
+
 void loop() {
 
-  cw(180);
-  ccw(180);
-  
+  read_joystick_data();
+  print_joystick_data();
+
+  servoH.writeMicroseconds(1500 + (xState/100.0*xSensitivity));  // Clockwise
+
+  delay(10);
+}
+
+void read_joystick_data()
+{
+  xState = map((int)analogRead(A5), 0, 1023, -100, 100);
+  yState = map((int)analogRead(A6), 0, 1023, -100, 100);
+
+  if (abs(xState) < 5)
+    xState = 0;
+  if (abs(yState) < 5)
+    yState = 0;
+}
+
+void print_joystick_data()
+{
+  Serial.print("X: ");
+  Serial.print(xState);
+  Serial.println();
+  Serial.print("Y: ");
+  Serial.print(yState);
+  Serial.println();
 }
