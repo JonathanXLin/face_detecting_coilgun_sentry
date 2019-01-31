306acb600d282dc8eeea787d2ff97169f7352342
Jonathan Lin <jlin1999@gmail.com>
2019-01-30 20:36:55 -0500

    Initial desktop winforms controller

---
+++ Code/Manual_Desktop/Winforms App/Sentry_Manual/Sentry_Manual/main.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Sentry_Manual
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.serialPortSentry = new System.IO.Ports.SerialPort(this.components);
+            this.comboBoxSerialPort = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Location = new System.Drawing.Point(446, 289);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(60, 60);
+            this.buttonDown.TabIndex = 1;
+            this.buttonDown.TabStop = false;
+            this.buttonDown.Text = "Down";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(512, 289);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(60, 60);
+            this.buttonRight.TabIndex = 2;
+            this.buttonRight.TabStop = false;
+            this.buttonRight.Text = "Right";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(380, 289);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(60, 60);
+            this.buttonLeft.TabIndex = 3;
+            this.buttonLeft.TabStop = false;
+            this.buttonLeft.Text = "Left";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(446, 223);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(60, 60);
+            this.buttonUp.TabIndex = 0;
+            this.buttonUp.TabStop = false;
+            this.buttonUp.Text = "Up";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // comboBoxSerialPort
+            // 
+            this.comboBoxSerialPort.FormattingEnabled = true;
+            this.comboBoxSerialPort.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7"});
+            this.comboBoxSerialPort.Location = new System.Drawing.Point(416, 60);
+            this.comboBoxSerialPort.Name = "comboBoxSerialPort";
+            this.comboBoxSerialPort.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSerialPort.TabIndex = 4;
+            this.comboBoxSerialPort.SelectedIndexChanged += new System.EventHandler(this.comboBoxSerialPort_SelectedIndexChanged);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.comboBoxSerialPort);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "main";
+            this.Text = "Sentry - Control";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.main_FormClosing);
+            this.Load += new System.EventHandler(this.main_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.main_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.main_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonUp;
+        private System.IO.Ports.SerialPort serialPortSentry;
+        private System.Windows.Forms.ComboBox comboBoxSerialPort;
+    }
+}
+
---
+++ Code/Manual_Desktop/Winforms App/Sentry_Manual/Sentry_Manual/main.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sentry_Manual
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+        private void main_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+            serialPortSentry.Open();
+        }
+
+        private void main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void main_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.W)
+            {
+                buttonUp.BackColor = Color.White;
+            }
+            if (e.KeyData == Keys.A)
+            {
+                buttonLeft.BackColor = Color.White;
+            }
+            if (e.KeyData == Keys.S)
+            {
+                buttonDown.BackColor = Color.White;
+            }
+            if (e.KeyData == Keys.D)
+            {
+                buttonRight.BackColor = Color.White;
+            }
+        }
+
+        private void main_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.W)
+                buttonUp.BackColor = default(Color);
+            if (e.KeyData == Keys.A)
+                buttonLeft.BackColor = default(Color);
+            if (e.KeyData == Keys.S)
+                buttonDown.BackColor = default(Color);
+            if (e.KeyData == Keys.D)
+                buttonRight.BackColor = default(Color);
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void up_press()
+        {
+            serialPortSentry.Write("test");
+        }
+
+        private void comboBoxSerialPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
